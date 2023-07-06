42c5bc2110f7ac4382ef42a476398f8fab5bebc2
Lenndame <138380100+Lenndame@users.noreply.github.com>
2023-07-06 22:46:49 +0200

    Update aufgabe_17_und_4_spiel.py
    variable anzahl an spielern und punkte system still wip

--- aufgabe_17_und_4_spiel.py
+++ aufgabe_17_und_4_spiel.py
@@ -1,60 +1,60 @@
-"""
-schwer
-
-Vereinfachte Version von 17 und 4, in der der Spieler nur gegen sich selbst
-spielt.
-Spieler zieht solange Karten, bis er entscheidet, das Spiel zu beenden oder sein 
-Kartenwert > 21 ist.
-Wenn der Gesamtwert seiner Karten <= 21 und > 17, hat der Spieler gewonnen.
-Anderenfalls hat er verloren.
-
-Kartenwerte
-2, 3, 4, 5, 6, 7, 8, 9, 10
-Bube, Dame, König: 10
-Ass 11
-
-shuffle methode aus dem random Modul
-es werden 4 Kartensätze in einem Spiel genutzt
-"""
-import random
-'''
-1. tatsächliches 17 + 4 einbauen
-2. Anzahl der Player einstellen 
-3. Gambling 
-'''
+mport random
 
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 colors = ["Pik", "Herz", "Kreuz", "Karo"]
 french_deck = [(color, value) for color in colors for value in values]
 
+play_deck = french_deck * 4
+
+players = {}
+
 playing = True
-# set_up = True
-dealer = 0
-player = 0
-cards = []
-# while set_up:
-#     player_count = int(input("wie viele spieler?: "))
-#     player_names = input("wie heißt der spieler?: ")
-#     for i in players:
-#     players = {}
+set_up = True
 
-random.shuffle(french_deck)
+hand = []
 
-while playing:   
-    print(cards)
-    action = input("+ für noch eine, - für keine karte mehr, q für quit ")
-    match action:
-        case "+":
-            card = french_deck.pop()
-            cards.append(card)
-            if sum(cards[1]) >= 22:
-                print("Verloren")
-        case "-":
-            if sum(cards([][1])) > 22:
-                print("Verloren")
-            if sum(cards([][1])) >= 17:
-                print("Gewonnen")
+while set_up:
+    gamestart = input("q zum Beenden ")
+    if gamestart == "q":
+        set_up = False
+    else:
+        player_count = int(input("Wie viele Spieler?: "))
+        player_names = input("Wie sind die Spielername?: ").split(",")
 
-        case "q":
-            playing = False
+        for n in range(player_count):
+            player_name = player_names[n]
+            players[player_name] = {"cards": [], "points": 0, "game_round": 0}
 
+    random.shuffle(french_deck)
+
+    while playing:
+        for player_name in players.keys():
+            if players[player_name]["game_round"] == 0:
+                print(f"{player_name} dein Zug!")
+
+            else:
+                print(f"{player_name} deine Karten {players[player_name]['cards']}")
+            action = input("+ gib mir eine Karte!, - Ich bleibe so!, q für quit ")
+            
+            match action:
+
+                case "+":
+                    card = french_deck.pop()
+                    players[player_name]["cards"].append(card)
+                    players[player_name]["game_round"] += 1
+
+                    print(f"Gezogene Karte:{card}")
+                    if sum(card[1] for card in players[player_name]["cards"]) >= 22:
+                        print("Sorry, du hast verloren!")
+                        players[player_name]["cards"].clear()
+
+                case "-":
+                    if sum(card[1] for card in players[player_name]["cards"]) >= 17:
+                        players[player_name]["cards"].clear()
+                        print("Herzlichen Glückwunsch, du hast gewonnen!")
+                        players[player_name]["points"] += 1
+                    if sum(card[1] for card in players[player_name]["cards"]) < 17:
+                        print("Du brauchst mindestens den Kartenwert 17!")
+
+                case "q":
+                    playing = False
